26bd7b838a89baca44595fab6828711931be50e1
debbysa <deby.silvia23@gmail.com>
2019-09-26 22:38:16 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// include router /admin
+app.use("/", require("./routes/gajiRouter"));
+
+// start server
+server.listen(4000, function() {
+  console.log("server 2 running on localhost:4000");
+});
